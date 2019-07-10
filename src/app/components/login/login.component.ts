2a10767cb16048565d3aba6c8c759d5a07fcbf97
pacquito <paco.felepia@gmail.com>
2019-07-10 11:57:45 -0500

    added login features

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { SESSION_STORAGE, WebStorageService } from 'angular-webstorage-service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username: string = "";
+  password: string = "";
 
+
+
+  //TODO: insert DB API endpoint here
+  url: string = "http://ec2-3-19-77-116.us-east-2.compute.amazonaws.com:8080/poketie/Login/";
+  //url: string = "http://localhost:8080/poketie/Login/"
+
+  constructor(private route: Router, @Inject(SESSION_STORAGE) private session: WebStorageService) { }
+  
   ngOnInit() {
   }
+
+  login(event){
+
+    let creds = [this.username,this.password]
+    var router = this.route
+    var sesh = this.session
+
+    if(this.username !== "" && this.password !== ""){
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", this.url);
+    xhr.onreadystatechange = function (){
+      if(this.readyState === 4 && this.status===200){
+        let autho = xhr.getResponseHeader("Authentication");
+        console.log("HERE IS THE AUTHO: " + autho)
+        sesh.set("token", autho);
+        //sessionStorage.setItem("token", autho);
+        //redirect here
+        console.log("THIS SHOULD BE THE AUTHO FROM SESSIONSTORAGE: "+sesh.get("token"))
+        router.navigate(['/backpack'])
+      } else if(this.readyState === 4 && this.status === 403){
+        alert("Invalid username or password")
+      }
+    }
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('Access-Control-Allow-Origin', router.url)
+    xhr.send(JSON.stringify(creds));
+  } else {
+    console.log("User tried inputting blank fields...");
+    alert("Please input into the username and password fields");
+  }
+  }
+
+
+
+
 
 }
